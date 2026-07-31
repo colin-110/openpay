cf1e482883419470aac4a646c7484a58a410e2d9
Colin K Thomas <colinkthomas110@gmail.com>
2026-07-31 18:12:18 +0530

    chore(outbox): drop an import left behind by the relay's locking change
    The relay stopped taking a Limit argument when it moved to FOR UPDATE SKIP
    LOCKED, but the import stayed.

--- libs/common-outbox/src/main/java/com/openpay/outbox/OutboxRelay.java
+++ libs/common-outbox/src/main/java/com/openpay/outbox/OutboxRelay.java
@@ -5,7 +5,6 @@ import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.Limit;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.transaction.annotation.Transactional;
